f5d49f9dc7c067f100eda4b5840738c1eddb3828
teddy1565 <teddy1565@gmail.com>
2022-01-12 22:44:39 +0800

    makemigrations & migrate, include args need careful

--- rest03/rest03/urls.py
+++ rest03/rest03/urls.py
@@ -20,11 +20,11 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r"users",views.UserViewSet)
-router.register(r"groups",views.GroupSerializer)
+router.register(r"groups",views.GroupViewSet)
 print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include(router.urls)),
-    path("api-auth/",include("rest_framework.urls"),namespace="rest_framework")
+    path("api-auth/",include("rest_framework.urls",namespace="rest_framework")),
 ]
