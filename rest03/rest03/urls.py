0cc0389bf7f5febd852545a4a9c3598d680beead
teddy1565 <teddy1565@gmail.com>
2022-01-12 22:49:40 +0800

    增加註解在LOG避免混淆

--- rest03/rest03/urls.py
+++ rest03/rest03/urls.py
@@ -21,10 +21,12 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register(r"users",views.UserViewSet)
 router.register(r"groups",views.GroupViewSet)
+print("-----show router.urls content-----")
 print(router.urls)
+print("-----split line-------")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include(router.urls)),
-    path("api-auth/",include("rest_framework.urls",namespace="rest_framework")),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
