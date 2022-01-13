5fff5432821af3f8369e30d3b34249171954249a
teddy1565 <teddy1565@gmail.com>
2022-01-13 17:31:39 +0800

    create super user & initial viewset

--- soft106/colorEnglish/views.py
+++ soft106/colorEnglish/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class GameHomePage(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "gameHomePage.html"
+
+    def get(self,request):
+        return Response()
