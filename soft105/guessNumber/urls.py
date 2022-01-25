0298aa3e5f5ac190b756fd5151835065ead29df2
teddy1565 <teddy1565@gmail.com>
2022-01-25 18:11:32 +0800

    soft105 guess number

--- soft105/guessNumber/urls.py
+++ soft105/guessNumber/urls.py
@@ -1,1 +1,11 @@
-urlpatterns = []
+from rest_framework import routers
+from .views import guessNumber,GeneratorNumber
+from django.urls import path,include
+
+router = routers.DefaultRouter()
+router.register(r"index",guessNumber,basename="index")
+router.register(r"generate",GeneratorNumber,basename="GeneratorNumber")
+
+urlpatterns = [
+    path("",include(router.urls)),
+]
--- soft105/guessNumber/views.py
+++ soft105/guessNumber/views.py
@@ -1,3 +1,40 @@
+import json
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.renderers import TemplateHTMLRenderer
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.template.context_processors import csrf
+from .models import GuessNumberModel
 # Create your views here.
+
+class guessNumber(viewsets.ModelViewSet):
+    queryset = GuessNumberModel.objects.none()
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "guess_num.html"
+    def list(self,request):
+        _csrf = {}
+        _csrf.update(csrf(request))
+        response = render(request,"guess_num.html")
+        return response
+    def create(self,request):
+        
+        try:
+            n = list(request.POST.keys())[0]
+            queryset = GuessNumberModel.objects.filter(num=int(n)).last()
+            if int(queryset.num) == int(n):
+                return HttpResponse("true")
+            else:
+                return HttpResponse("false")
+        except:
+            return HttpResponse("false")
+
+class GeneratorNumber(viewsets.ModelViewSet):
+    queryset = GuessNumberModel.objects.none()
+
+    def list(self,request):
+        try:
+            queryset = GuessNumberModel(num=int(request.GET["num"]))
+            queryset.save()
+            return HttpResponse("success")
+        except:
+            return HttpResponseBadRequest("Can't Query SQL")
